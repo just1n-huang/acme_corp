17763f955ce333436136d53cb16797937aeba62b
Justin Huang <justin88@gmail.com>
2022-08-09 01:32:05 -0400

    added routes

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ app.get("/api/departments", async (req, res, next) => {
   }
 });
 
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    res.send(
+      await Employee.findAll({
+        include: [
+          {
+            model: Employee,
+            as: "supervisor",
+          },
+          Employee,
+          Department,
+        ],
+      })
+    );
+  } catch (ex) {
+    next(ex);
+  }
+});
+
 const Department = conn.define("department", {
   name: {
     type: STRING(20),
@@ -37,16 +56,23 @@ const Employee = conn.define("employee", {
 Department.belongsTo(Employee, { as: "manager" });
 Employee.hasMany(Department, { foreignKey: "managerId" });
 
+Employee.belongsTo(Employee, { as: "supervisor" });
+Employee.hasMany(Employee, { foreignKey: "supervisorId" });
+
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
-  const [moe, lucy, hr, engineering] = await Promise.all([
+  const [moe, lucy, larry, hr, engineering] = await Promise.all([
     Employee.create({ name: "moe" }),
     Employee.create({ name: "lucy" }),
+    Employee.create({ name: "larry" }),
     Department.create({ name: "hr" }),
     Department.create({ name: "engineering" }),
   ]);
   hr.managerId = lucy.id;
   await hr.save();
+  moe.supervisorId = lucy.id;
+  larry.supervisorId = lucy.id;
+  await Promise.all([moe.save(), larry.save()]);
 };
 
 const init = async () => {
