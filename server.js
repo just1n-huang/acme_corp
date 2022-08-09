eaade45b26105d0941983da193f7817bd1e5d3ab
Justin Huang <justin88@gmail.com>
2022-08-09 01:42:21 -0400

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const Sequelize = require("sequelize");
-const { STRING, UUID, UUIDV4 } = Sequelize;
-const conn = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/acme_db"
-);
+// const Sequelize = require("sequelize");
+// const { STRING, UUID, UUIDV4 } = Sequelize;
+// const conn = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost/acme_db"
+// );
 const express = require("express");
 const app = express();
+
+const {
+  conn,
+  syncAndSeed,
+  models: { Department, Employee },
+} = require("./db");
 
 app.get("/api/departments", async (req, res, next) => {
   try {
@@ -37,43 +43,43 @@ app.get("/api/employees", async (req, res, next) => {
   }
 });
 
-const Department = conn.define("department", {
-  name: {
-    type: STRING(20),
-  },
-});
-const Employee = conn.define("employee", {
-  id: {
-    type: UUID,
-    primaryKey: true,
-    defaultValue: UUIDV4,
-  },
-  name: {
-    type: STRING(20),
-  },
-});
+// const Department = conn.define("department", {
+//   name: {
+//     type: STRING(20),
+//   },
+// });
+// const Employee = conn.define("employee", {
+//   id: {
+//     type: UUID,
+//     primaryKey: true,
+//     defaultValue: UUIDV4,
+//   },
+//   name: {
+//     type: STRING(20),
+//   },
+// });
 
-Department.belongsTo(Employee, { as: "manager" });
-Employee.hasMany(Department, { foreignKey: "managerId" });
+// Department.belongsTo(Employee, { as: "manager" });
+// Employee.hasMany(Department, { foreignKey: "managerId" });
 
-Employee.belongsTo(Employee, { as: "supervisor" });
-Employee.hasMany(Employee, { foreignKey: "supervisorId" });
+// Employee.belongsTo(Employee, { as: "supervisor" });
+// Employee.hasMany(Employee, { foreignKey: "supervisorId" });
 
-const syncAndSeed = async () => {
-  await conn.sync({ force: true });
-  const [moe, lucy, larry, hr, engineering] = await Promise.all([
-    Employee.create({ name: "moe" }),
-    Employee.create({ name: "lucy" }),
-    Employee.create({ name: "larry" }),
-    Department.create({ name: "hr" }),
-    Department.create({ name: "engineering" }),
-  ]);
-  hr.managerId = lucy.id;
-  await hr.save();
-  moe.supervisorId = lucy.id;
-  larry.supervisorId = lucy.id;
-  await Promise.all([moe.save(), larry.save()]);
-};
+// const syncAndSeed = async () => {
+//   await conn.sync({ force: true });
+//   const [moe, lucy, larry, hr, engineering] = await Promise.all([
+//     Employee.create({ name: "moe" }),
+//     Employee.create({ name: "lucy" }),
+//     Employee.create({ name: "larry" }),
+//     Department.create({ name: "hr" }),
+//     Department.create({ name: "engineering" }),
+//   ]);
+//   hr.managerId = lucy.id;
+//   await hr.save();
+//   moe.supervisorId = lucy.id;
+//   larry.supervisorId = lucy.id;
+//   await Promise.all([moe.save(), larry.save()]);
+// };
 
 const init = async () => {
   try {
